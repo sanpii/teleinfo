fc93357f583a09cd9ce03aed1f05b4f045cff238
Sanpi <sanpi@homecomputing.fr>
2016-08-08 12:33:24 +0200

    Pass file path as parameter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+extern crate docopt;
+extern crate rustc_serialize;
+
+use docopt::Docopt;
+
 mod teleinfo;
+
+static USAGE: &'static str = "Usage: ws2300 <device>";
+
+#[derive(RustcDecodable)]
+struct Args
+{
+    arg_device: String,
+}
 
 fn main()
 {
+    let docopt = match Docopt::new(USAGE) {
+        Ok(d) => d,
+        Err(e) => e.exit(),
+    };
+
+    let args: Args = match docopt.decode() {
+        Ok(args) => args,
+        Err(e) => e.exit(),
+    };
+
     let parser = teleinfo::Parser::new();
-    let frame = parser.read_frame(String::from("teleinfo.txt"));
+    let frame = parser.read_frame(args.arg_device);
     let data = parser.parse(frame);
 
     println!("{:?}", data);
